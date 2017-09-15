ec1b1d3dc3eab0ec0ecb363e4971bd10490621d4
limn2o4 <674902997@qq.com>
2017-09-15 13:06:49 +0800

    silde window check

--- light-check.py
+++ light-check.py
@@ -23,7 +23,18 @@ _,b_th = cv2.threshold(b,200,255,cv2.THRESH_BINARY)
 _,g_th = cv2.threshold(g,200,255,cv2.THRESH_BINARY)
 _,r_th = cv2.threshold(r,200,255,cv2.THRESH_BINARY)
 res = g_th - r_th;
-
+ker = 5;
+print(res.shape,h,w)
+for i in range(0,h - 5,5):
+    for j in range(0,w - 5,5):
+        cnt = 0
+        for k in range(5):
+            for l in range(5):
+                if res[i+k,j+l] != 0:
+                    cnt += 1
+        if cnt >=3: 
+            print(cnt,i,j)
+            cv2.rectangle(img,(j,i),(j+5,i+5),[255,255,255])
 cv2.imshow("g",res)
-cv2.imshow("123",)
+cv2.imshow("res",img)
 cv2.waitKey(0)
