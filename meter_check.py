07f0833ebb357167daa94d66260e444d77544da9
limn2o4 <674902997@qq.com>
2017-09-22 13:24:30 +0800

    add accuarcy

--- meter_check.py
+++ meter_check.py
@@ -14,20 +14,22 @@ def minfilter(image,ksize = 3):
     return dst
 
 
-img = cv2.imread(r"D:\project\deep-meter\meter1.jpg")
+img = cv2.imread(r"D:\project\deep-meter\meter2.jpg")
 gary = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-binary = cv2.adaptiveThreshold(gary,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,3,10)
-mn = minfilter(binary,3)
-#cv2.imshow("out",mn)
+binary = cv2.adaptiveThreshold(gary,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,7,10)
+#mn = minfilter(binary,3)
+
 binary = cv2.blur(binary,(3,3))
+cv2.imshow("out",binary)
 x = cv2.Sobel(binary,cv2.CV_16S,1,0)
 y = cv2.Sobel(binary,cv2.CV_16S,0,1)
 absX = cv2.convertScaleAbs(x)
 absY = cv2.convertScaleAbs(y)
 edges = cv2.addWeighted(absX,0.5,absY,0.5,0)
 #use soble not canny
-lines = cv2.HoughLinesP(edges,1,np.pi/180,100,300,10)
-circles = cv2.HoughCircles(gary,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=50)
+#edges = cv2.Canny(binary,100,300)
+
+circles = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT,10,200,param1=50,param2=50)
 radius = 0
 maxx,maxy = 0,0
 for i in circles[0,:]:
@@ -40,12 +42,15 @@ for i in circles[0,:]:
     # draw the center of the circle
     #cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
 cv2.circle(img,(maxx,maxy),radius,(0,255,0),2)
-cv2.imshow("333",edges)
+roi = edges[int(maxx - radius):int(maxx + radius),int(maxy-radius):int(maxy+radius)]
+cv2.imshow("1234",roi)
+cv2.waitKey(0)
+lines = cv2.HoughLinesP(roi,1,np.pi/180,100,300,20)
 for x1,y1,x2,y2 in lines[0]:
     #print(line[0][1],line[0][0])
     #x1,y1,x2,y2 = lis[0][0],lis[0][1],lis[0][2],lis[0][3]
    # print(x1,y1,x2,y2)
-    print(np.arctan((x1 - x2)/(y1 - y2)))
+    print(180/np.pi*np.arctan((x1 - x2)/(y1 - y2)))
     cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
 cv2.imshow("123",img)
